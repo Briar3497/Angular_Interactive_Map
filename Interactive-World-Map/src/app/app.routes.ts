5c161dd5538e1375d99c09af31a6f7d8b7deee66
Briar Lilly <silvermoon3497@gmail.com>
2026-01-20 16:10:38 -0600

    Fixes README.md and initializes angular app

--- Interactive-World-Map/src/app/app.routes.ts
+++ Interactive-World-Map/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+<<<<<<< HEAD
 import { SVG } from './svg/svg';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/map', pathMatch: 'full' },
     { path: 'map', component: SVG },
 ];
+=======
+
+export const routes: Routes = [];
+>>>>>>> 155e83e (Fixes README.md and initializes angular app)
