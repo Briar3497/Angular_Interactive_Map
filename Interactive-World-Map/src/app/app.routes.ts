8049009011e0eef25a9f30ccdaf06f2de1ac2fb8
Briar Lilly <silvermoon3497@gmail.com>
2026-01-20 18:42:45 -0600

    finishes part D

--- Interactive-World-Map/src/app/app.routes.ts
+++ Interactive-World-Map/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { SVG } from './svg/svg';
 
 export const routes: Routes = [
@@ -10,3 +11,11 @@ export const routes: Routes = [
 
 export const routes: Routes = [];
 >>>>>>> 155e83e (Fixes README.md and initializes angular app)
+=======
+import { SVG } from './svg/svg';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/map', pathMatch: 'full' },
+    { path: 'map', component: SVG },
+];
+>>>>>>> 4ea14e4 (finishes part D)
