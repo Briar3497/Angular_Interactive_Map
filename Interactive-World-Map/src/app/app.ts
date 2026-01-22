af03592120f868b3e91d98d37224c094b4c149a9
Briar Lilly <silvermoon3497@gmail.com>
2026-01-22 15:59:53 -0600

    adds svg element and finishes part E

--- Interactive-World-Map/src/app/app.ts
+++ Interactive-World-Map/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SVG } from './svg/svg';
+import { WorldBank } from './world-bank';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SVG],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
