5f3c110bae694908f0ff0eed7715bdee845f8de6
Briar Lilly <silvermoon3497@gmail.com>
2026-01-23 21:12:00 -0600

    fixes lingering bugs and finishes part C, F, and G

--- Interactive-World-Map/src/app/world-bank.ts
+++ Interactive-World-Map/src/app/world-bank.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WorldBank {
-  
+  constructor(private http: HttpClient) {}
+
+  getCountryData(countryCode: string): Observable<any> {
+    const url = `https://api.worldbank.org/v2/country/${countryCode}?format=json`;
+    console.log("Fetching data for country code:", countryCode);
+    return this.http.get(url).pipe(map((response: any) => response[1][0]));
+  }
 }
