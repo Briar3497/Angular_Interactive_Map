1d5d41e2db7254d0092bde50c1016a35ac916458
Briar Lilly <silvermoon3497@gmail.com>
2026-01-23 21:12:00 -0600

    fixes lingering bugs and finishes part C, F, and G

--- Interactive-World-Map/src/app/svg/svg.ts
+++ Interactive-World-Map/src/app/svg/svg.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, output} from '@angular/core';
+import { WorldBank } from '../world-bank';
 
 @Component({
   selector: 'app-svg',
+  standalone: true,
   imports: [],
   templateUrl: './svg.html',
   styleUrl: './svg.css',
 })
 export class SVG {
+  countrySelected = output<string>();
+  target: any;
 
-}
+  handleCountryClick(event: MouseEvent) {
+    this.target = event.target as SVGElement;
+    console.log('Country clicked:', this.target.id);
+    this.countrySelected.emit(this.target.id);
+  }
+};
