1eccf52c3803d56e41fedace7c303986211ffc60
Briar Lilly <silvermoon3497@gmail.com>
2026-01-23 21:12:00 -0600

    fixes lingering bugs and finishes part C, F, and G

--- Interactive-World-Map/src/app/app.ts
+++ Interactive-World-Map/src/app/app.ts
@@ -1,14 +1,42 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SVG } from './svg/svg';
 import { WorldBank } from './world-bank';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SVG],
+  standalone: true,
+  imports: [SVG, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Interactive-World-Map');
+  countryInfo = signal<any>({
+    name: '',
+    capitalCity: '',
+    region: '',
+    incomeLevel: '',
+    longitude: '',
+    latitude: ''
+  });
+
+  constructor(private worldBank: WorldBank) {}
+
+  onCountrySelected(countryCode: any) {
+    this.worldBank.getCountryData(countryCode).subscribe((response: any) => {
+      if (response) {
+        console.log(response);
+        this.countryInfo.set({
+          name: response.name,
+          capitalCity: response.capitalCity,
+          region: response.region.value,
+          incomeLevel: response.incomeLevel.value,
+          longitude: response.longitude,
+          latitude: response.latitude
+        });
+      }
+      console.log('Country info updated:', this.countryInfo);
+    });
+  }
 }
